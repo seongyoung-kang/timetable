51bc008b1729f5d1fa2dc34a5d40d4d146df2c7f
guny524 <guny524@kookmin.ac.kr>
2016-11-08 19:26:08 +0900

    adding build file for check

---
+++ build/ui_mainwindow.h
@@ -0,0 +1,365 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.7.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QListWidget>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QTabWidget *tabWidget;
+    QWidget *tab_1;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout_1;
+    QLabel *label_1;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
+    QLabel *label_6;
+    QLabel *label_7;
+    QLabel *label_8;
+    QLabel *label_9;
+    QLabel *label_10;
+    QLabel *label_11;
+    QLabel *label_12;
+    QWidget *horizontalLayoutWidget;
+    QHBoxLayout *horizontalLayout_1;
+    QLabel *label_sun;
+    QLabel *label_mon;
+    QLabel *label_tus;
+    QLabel *label_whe;
+    QLabel *label_thi;
+    QLabel *label_fri;
+    QLabel *label_sat;
+    QFrame *line_2;
+    QFrame *line_3;
+    QFrame *line_4;
+    QFrame *line_5;
+    QFrame *line_7;
+    QFrame *line_8;
+    QFrame *line_9;
+    QFrame *line_10;
+    QFrame *line_11;
+    QFrame *line_12;
+    QFrame *line_13;
+    QFrame *line_14;
+    QFrame *line_6;
+    QFrame *mainFrame;
+    QListWidget *listWidget_2;
+    QWidget *tab_2;
+    QListWidget *listWidget;
+    QLineEdit *lineEdit;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(950, 670);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        tabWidget = new QTabWidget(centralWidget);
+        tabWidget->setObjectName(QStringLiteral("tabWidget"));
+        tabWidget->setGeometry(QRect(10, 0, 950, 630));
+        tab_1 = new QWidget();
+        tab_1->setObjectName(QStringLiteral("tab_1"));
+        verticalLayoutWidget = new QWidget(tab_1);
+        verticalLayoutWidget->setObjectName(QStringLiteral("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(50, 50, 21, 481));
+        verticalLayout_1 = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout_1->setSpacing(6);
+        verticalLayout_1->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_1->setObjectName(QStringLiteral("verticalLayout_1"));
+        verticalLayout_1->setContentsMargins(0, 0, 0, 0);
+        label_1 = new QLabel(verticalLayoutWidget);
+        label_1->setObjectName(QStringLiteral("label_1"));
+        label_1->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_1->addWidget(label_1);
+
+        label_2 = new QLabel(verticalLayoutWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_1->addWidget(label_2);
+
+        label_3 = new QLabel(verticalLayoutWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_1->addWidget(label_3);
+
+        label_4 = new QLabel(verticalLayoutWidget);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_1->addWidget(label_4);
+
+        label_5 = new QLabel(verticalLayoutWidget);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_1->addWidget(label_5);
+
+        label_6 = new QLabel(verticalLayoutWidget);
+        label_6->setObjectName(QStringLiteral("label_6"));
+        label_6->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_1->addWidget(label_6);
+
+        label_7 = new QLabel(verticalLayoutWidget);
+        label_7->setObjectName(QStringLiteral("label_7"));
+        label_7->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_1->addWidget(label_7);
+
+        label_8 = new QLabel(verticalLayoutWidget);
+        label_8->setObjectName(QStringLiteral("label_8"));
+        label_8->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_1->addWidget(label_8);
+
+        label_9 = new QLabel(verticalLayoutWidget);
+        label_9->setObjectName(QStringLiteral("label_9"));
+        label_9->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_1->addWidget(label_9);
+
+        label_10 = new QLabel(verticalLayoutWidget);
+        label_10->setObjectName(QStringLiteral("label_10"));
+        label_10->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_1->addWidget(label_10);
+
+        label_11 = new QLabel(verticalLayoutWidget);
+        label_11->setObjectName(QStringLiteral("label_11"));
+        label_11->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_1->addWidget(label_11);
+
+        label_12 = new QLabel(verticalLayoutWidget);
+        label_12->setObjectName(QStringLiteral("label_12"));
+        label_12->setAlignment(Qt::AlignCenter);
+
+        verticalLayout_1->addWidget(label_12);
+
+        horizontalLayoutWidget = new QWidget(tab_1);
+        horizontalLayoutWidget->setObjectName(QStringLiteral("horizontalLayoutWidget"));
+        horizontalLayoutWidget->setGeometry(QRect(70, 20, 631, 31));
+        horizontalLayout_1 = new QHBoxLayout(horizontalLayoutWidget);
+        horizontalLayout_1->setSpacing(6);
+        horizontalLayout_1->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_1->setObjectName(QStringLiteral("horizontalLayout_1"));
+        horizontalLayout_1->setContentsMargins(0, 0, 0, 0);
+        label_sun = new QLabel(horizontalLayoutWidget);
+        label_sun->setObjectName(QStringLiteral("label_sun"));
+        label_sun->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout_1->addWidget(label_sun);
+
+        label_mon = new QLabel(horizontalLayoutWidget);
+        label_mon->setObjectName(QStringLiteral("label_mon"));
+        label_mon->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout_1->addWidget(label_mon);
+
+        label_tus = new QLabel(horizontalLayoutWidget);
+        label_tus->setObjectName(QStringLiteral("label_tus"));
+        label_tus->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout_1->addWidget(label_tus);
+
+        label_whe = new QLabel(horizontalLayoutWidget);
+        label_whe->setObjectName(QStringLiteral("label_whe"));
+        label_whe->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout_1->addWidget(label_whe);
+
+        label_thi = new QLabel(horizontalLayoutWidget);
+        label_thi->setObjectName(QStringLiteral("label_thi"));
+        label_thi->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout_1->addWidget(label_thi);
+
+        label_fri = new QLabel(horizontalLayoutWidget);
+        label_fri->setObjectName(QStringLiteral("label_fri"));
+        label_fri->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout_1->addWidget(label_fri);
+
+        label_sat = new QLabel(horizontalLayoutWidget);
+        label_sat->setObjectName(QStringLiteral("label_sat"));
+        label_sat->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout_1->addWidget(label_sat);
+
+        line_2 = new QFrame(tab_1);
+        line_2->setObjectName(QStringLiteral("line_2"));
+        line_2->setGeometry(QRect(70, 170, 630, 3));
+        line_2->setFrameShape(QFrame::HLine);
+        line_2->setFrameShadow(QFrame::Sunken);
+        line_3 = new QFrame(tab_1);
+        line_3->setObjectName(QStringLiteral("line_3"));
+        line_3->setGeometry(QRect(70, 290, 630, 3));
+        line_3->setFrameShape(QFrame::HLine);
+        line_3->setFrameShadow(QFrame::Sunken);
+        line_4 = new QFrame(tab_1);
+        line_4->setObjectName(QStringLiteral("line_4"));
+        line_4->setGeometry(QRect(70, 410, 630, 3));
+        line_4->setFrameShape(QFrame::HLine);
+        line_4->setFrameShadow(QFrame::Sunken);
+        line_5 = new QFrame(tab_1);
+        line_5->setObjectName(QStringLiteral("line_5"));
+        line_5->setGeometry(QRect(70, 530, 630, 3));
+        line_5->setFrameShape(QFrame::HLine);
+        line_5->setFrameShadow(QFrame::Sunken);
+        line_7 = new QFrame(tab_1);
+        line_7->setObjectName(QStringLiteral("line_7"));
+        line_7->setGeometry(QRect(250, 50, 3, 480));
+        line_7->setFrameShape(QFrame::VLine);
+        line_7->setFrameShadow(QFrame::Sunken);
+        line_8 = new QFrame(tab_1);
+        line_8->setObjectName(QStringLiteral("line_8"));
+        line_8->setGeometry(QRect(160, 50, 3, 480));
+        line_8->setFrameShape(QFrame::VLine);
+        line_8->setFrameShadow(QFrame::Sunken);
+        line_9 = new QFrame(tab_1);
+        line_9->setObjectName(QStringLiteral("line_9"));
+        line_9->setGeometry(QRect(340, 50, 3, 480));
+        line_9->setFrameShape(QFrame::VLine);
+        line_9->setFrameShadow(QFrame::Sunken);
+        line_10 = new QFrame(tab_1);
+        line_10->setObjectName(QStringLiteral("line_10"));
+        line_10->setGeometry(QRect(430, 50, 3, 480));
+        line_10->setFrameShape(QFrame::VLine);
+        line_10->setFrameShadow(QFrame::Sunken);
+        line_11 = new QFrame(tab_1);
+        line_11->setObjectName(QStringLiteral("line_11"));
+        line_11->setGeometry(QRect(520, 50, 3, 480));
+        line_11->setFrameShape(QFrame::VLine);
+        line_11->setFrameShadow(QFrame::Sunken);
+        line_12 = new QFrame(tab_1);
+        line_12->setObjectName(QStringLiteral("line_12"));
+        line_12->setGeometry(QRect(610, 50, 3, 480));
+        line_12->setFrameShape(QFrame::VLine);
+        line_12->setFrameShadow(QFrame::Sunken);
+        line_13 = new QFrame(tab_1);
+        line_13->setObjectName(QStringLiteral("line_13"));
+        line_13->setGeometry(QRect(700, 50, 3, 480));
+        line_13->setFrameShape(QFrame::VLine);
+        line_13->setFrameShadow(QFrame::Sunken);
+        line_14 = new QFrame(tab_1);
+        line_14->setObjectName(QStringLiteral("line_14"));
+        line_14->setGeometry(QRect(70, 50, 3, 480));
+        line_14->setFrameShape(QFrame::VLine);
+        line_14->setFrameShadow(QFrame::Sunken);
+        line_6 = new QFrame(tab_1);
+        line_6->setObjectName(QStringLiteral("line_6"));
+        line_6->setGeometry(QRect(70, 50, 630, 3));
+        line_6->setFrameShape(QFrame::HLine);
+        line_6->setFrameShadow(QFrame::Sunken);
+        mainFrame = new QFrame(tab_1);
+        mainFrame->setObjectName(QStringLiteral("mainFrame"));
+        mainFrame->setGeometry(QRect(70, 50, 630, 480));
+        mainFrame->setFrameShape(QFrame::StyledPanel);
+        mainFrame->setFrameShadow(QFrame::Raised);
+        listWidget_2 = new QListWidget(tab_1);
+        listWidget_2->setObjectName(QStringLiteral("listWidget_2"));
+        listWidget_2->setGeometry(QRect(720, 20, 200, 510));
+        tabWidget->addTab(tab_1, QString());
+        tab_2 = new QWidget();
+        tab_2->setObjectName(QStringLiteral("tab_2"));
+        listWidget = new QListWidget(tab_2);
+        listWidget->setObjectName(QStringLiteral("listWidget"));
+        listWidget->setGeometry(QRect(50, 40, 581, 531));
+        lineEdit = new QLineEdit(tab_2);
+        lineEdit->setObjectName(QStringLiteral("lineEdit"));
+        lineEdit->setGeometry(QRect(720, 110, 113, 21));
+        tabWidget->addTab(tab_2, QString());
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 950, 26));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        tabWidget->setCurrentIndex(0);
+
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+#ifndef QT_NO_TOOLTIP
+        tabWidget->setToolTip(QApplication::translate("MainWindow", "<html><head/><body><p>erwefwf</p></body></html>", 0));
+#endif // QT_NO_TOOLTIP
+        label_1->setText(QApplication::translate("MainWindow", "9", 0));
+        label_2->setText(QApplication::translate("MainWindow", "10", 0));
+        label_3->setText(QApplication::translate("MainWindow", "11", 0));
+        label_4->setText(QApplication::translate("MainWindow", "12", 0));
+        label_5->setText(QApplication::translate("MainWindow", "13", 0));
+        label_6->setText(QApplication::translate("MainWindow", "14", 0));
+        label_7->setText(QApplication::translate("MainWindow", "15", 0));
+        label_8->setText(QApplication::translate("MainWindow", "16", 0));
+        label_9->setText(QApplication::translate("MainWindow", "17", 0));
+        label_10->setText(QApplication::translate("MainWindow", "18", 0));
+        label_11->setText(QApplication::translate("MainWindow", "19", 0));
+        label_12->setText(QApplication::translate("MainWindow", "20", 0));
+        label_sun->setText(QApplication::translate("MainWindow", "SUN", 0));
+        label_mon->setText(QApplication::translate("MainWindow", "MON", 0));
+        label_tus->setText(QApplication::translate("MainWindow", "TUS", 0));
+        label_whe->setText(QApplication::translate("MainWindow", "WHE", 0));
+        label_thi->setText(QApplication::translate("MainWindow", "THI", 0));
+        label_fri->setText(QApplication::translate("MainWindow", "FRI", 0));
+        label_sat->setText(QApplication::translate("MainWindow", "SAT", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_1), QApplication::translate("MainWindow", "Tab 1", 0));
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QApplication::translate("MainWindow", "Tab 2", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
